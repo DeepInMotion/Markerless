d9a2d6225ed109c52fc7c5ee5a20d03951b5442e
Daniel Groos <daniel.groos@ntnu.no>
2021-03-03 07:29:38 +0100

    Minor corrections in model script

--- models/EfficientHourglass.py
+++ models/EfficientHourglass.py
@@ -66,7 +66,7 @@ class architecture:
         elif(self.model_type == 'X'): #TO BE GENERATED
             efficientnet_module = efficientnet_X.efficientnet_lite(self.efficientnet_variant, input_resolution=self.input_resolution).model #TO BE GENERATED 
             input_layer = efficientnet_module.layers[0].input
-            #self.efficientnet_features = {0: {'res1': 25, 'res2': 42, 'res3': 94, 'res4': 129}, 1: {'res1': 34, 'res2': 60, 'res3': 130, 'res4': 174}, 2: {'res1': 34, 'res2': 60, 'res3': 130, 'res4': 174}, 3: {'res1': 34, 'res2': 60, 'res3': 148, 'res4': 201}, 4: {'res1': 43, 'res2': 78, 'res3': 184, 'res4': 255}}.[self.efficientnet_variant]
+            #self.efficientnet_features = {0: {'res1': 25, 'res2': 42, 'res3': 94, 'res4': 129}, 1: {'res1': 34, 'res2': 60, 'res3': 130, 'res4': 174}, 2: {'res1': 34, 'res2': 60, 'res3': 130, 'res4': 174}, 3: {'res1': 34, 'res2': 60, 'res3': 148, 'res4': 201}, 4: {'res1': 43, 'res2': 78, 'res3': 184, 'res4': 255}}[self.efficientnet_variant]
         else:
             input_shape = (self.input_resolution, self.input_resolution, 3)
             input_layer = Input(shape=input_shape, name='input_res1')
@@ -122,7 +122,7 @@ class architecture:
             Conf_maps = deconvX
         self.model_outputs.append(Conf_maps) 
         
-        # Build CIMA-Pose model
+        # Build EfficientHourglass model
         self.model = Model(input_layer, self.model_outputs, name='EfficientHourglass')
 
     def Transpose_concat_squeeze(self, feature_maps1, feature_maps2, filters, se_ratio, trainable):
